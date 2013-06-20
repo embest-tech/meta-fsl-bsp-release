faca55869827c54a102c1cf1316eef4bde490e81
Lauren Post <lauren.post@freescale.com>
2013-06-20 11:28:59 -0500

    Update release setup script to include build directory and backend parameters
    Signed-off-by: Lauren Post <lauren.post@freescale.com>

--- imx/tools/fsl-setup-release.sh
+++ imx/tools/fsl-setup-release.sh
@@ -18,6 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+CWD=`pwd`
 
 exit_message ()
 {
@@ -25,41 +26,93 @@ exit_message ()
    echo "    source setup-environment build"
 }
 
+usage()
+{
+    echo -e "\nUsage: source $PROGNAME
+    Optional parameters: [-b build-dir] [-e back-end] [-h]"
+echo "
+    * [-b build-dir]: Build directory, if unspecified script uses 'build' as output directory
+    * [-e back-end]: Options are 'fb', 'dfb', weston'. If unspecified, default to X11
+    * [-h]: help
+
+"
+}
+
+
 clean_up()
 {
+
+    unset CWD BUILD_DIR BACKEND DIST_FEATURES
+    unset fsl_setup_help fsl_setup_error fsl_setup_flag
+    unset usage clean_up
     unset ARM_DIR META_FSL_BSP_RELEASE
     exit_message clean_up
 }
 
-# ARM_DIR is the directory that script fsl_setup exists
-ARM_DIR=`readlink -f ${BASH_SOURCE[0]}`
-ARM_DIR=`dirname $ARM_DIR`
+# get command line options
+OLD_OPTIND=$OPTIND
+while getopts "k:r:t:b:e:gh" fsl_setup_flag
+do
+    case $fsl_setup_flag in
+        b) BUILD_DIR="$OPTARG";
+           ;;
+        e)
+            BACKEND="$OPTARG"
+            if [ "$BACKEND" = "fb" ]; then
+                DIST_FEATURES="alsa argp bluetooth ext2 irda largefile pcmcia usbgadget usbhost wifi xattr nfs zeroconf pci 3g \${DISTRO_FEATURES_LIBC}"
+            elif [ "$BACKEND" = "dfb" ]; then
+                DIST_FEATURES="alsa argp bluetooth ext2 irda largefile pcmcia usbgadget usbhost wifi xattr nfs zeroconf pci 3g directfb \${DISTRO_FEATURES_LIBC}"
+            else
+                echo -e "\nWRONG BACKEND SPECIFIED"
+                fsl_setup_error='true'
+            fi
+           ;;
+        h) fsl_setup_help='true';
+           ;;
+        ?) fsl_setup_error='true';
+           ;;
+    esac
+done
+OPTIND=$OLD_OPTIND
 
-META_FSL_BSP_RELEASE="${ARM_DIR}/sources/meta-fsl-bsp-release/imx/meta-fsl-arm"
-echo "##Freescale Yocto Release layer" >> build/conf/bblayers.conf
-echo "BBLAYERS += \"${META_FSL_BSP_RELEASE}\"" >> build/conf/bblayers.conf
+# check the "-h" and other not supported options
+if test $fsl_setup_error || test $fsl_setup_help; then
+    usage && clean_up && return 1
+fi
 
-META_FSL_BSP_RELEASE="${ARM_DIR}/sources/meta-fsl-bsp-release/imx/meta-fsl-demos"
-echo "BBLAYERS += \"${META_FSL_BSP_RELEASE}\"" >> build/conf/bblayers.conf
+if [ -z "$BUILD_DIR" ]; then
+    BUILD_DIR='build'
+fi
+if [ ! -e $BUILD_DIR/conf/local.conf ]; then
+    echo "Run the 'setup-environment' script before running this script"
+    clean_up && return 1
+fi
 
+# Run this part only the first time the script is executed
+if [ ! -e $BUILD_DIR/conf/local.conf.org ]; then
+    cp $BUILD_DIR/conf/local.conf $BUILD_DIR/conf/local.conf.org
 
-# setup external build servers
-echo "# Freescale Build servers" >> build/conf/local.conf
-echo "FSL_ARM_GIT_SERVER = \"git.freescale.com/imx\"" >> build/conf/local.conf
-echo >> build/conf/local.conf
+    META_FSL_BSP_RELEASE="${CWD}/sources/meta-fsl-bsp-release/imx/meta-fsl-arm"
+    echo "##Freescale Yocto Release layer" >> $BUILD_DIR/conf/bblayers.conf
+    echo "BBLAYERS += \"${META_FSL_BSP_RELEASE}\"" >> $BUILD_DIR/conf/bblayers.conf
 
-echo "# Freescale Download - optimize for disk space " >> build/conf/local.conf
-mkdir /opt/freescale/yocto/imx/download
-echo "DL_DIR = \"/opt/freescale/yocto/imx/download\"" >> build/conf/local.conf
-echo >> build/conf/local.conf
+    META_FSL_BSP_RELEASE="${CWD}/sources/meta-fsl-bsp-release/imx/meta-fsl-demos"
+    echo "BBLAYERS += \"${META_FSL_BSP_RELEASE}\"" >> $BUILD_DIR/conf/bblayers.conf
 
-echo "# Freescale SSTATE_CACHE - optimize for build time and disk space" >> build/conf/local.conf
-mkdir /opt/freescale/yocto/imx/sstate-cache
-echo "SSTATE_CACHE = \"/opt/freescale/yocto/imx/sstate-cache\"" >> build/conf/local.conf
-echo >> build/conf/local.conf
+    echo >> $BUILD_DIR/conf/local.conf
 
+    # setup external build servers used in uboot and kernel recipes
+    echo "# Freescale Build servers" >> $BUILD_DIR/conf/local.conf
+    echo "FSL_ARM_GIT_SERVER = \"git.freescale.com/imx\"" >> $BUILD_DIR/conf/local.conf
+    echo >> $BUILD_DIR/conf/local.conf
+
+    if [ -e "$BACKEND" ]; then
+        echo "DISTRO_FEATURES = \"$DIST_FEATURES\"" >> $BUILD_DIR/conf/local.conf
+        echo >> $BUILD_DIR/conf/local.conf
+    fi
+fi
 
 
 exit_message
-cd build
+cd  $BUILD_DIR
 clean_up
